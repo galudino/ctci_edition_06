863d8f10f917263d9d06fbc779e335edd9c515c4
Gemuele Aludino <g.aludino@gmail.com>
2021-01-21 20:56:33 -0500

    Sources for question 1.1
    There are three answers for this question:
    
    Brute force using nested `for-loops`:
    ```
    bool has_unique_chars(const std::string &str)
    ```
    
    Uses a `std::vector<bool>(128) v` to determine if `v[str[i]] == 1`, which means the character has occured at least once. If `v[str[i]] == 0`, we set `v[str[i]]` to 1.
    Don't worry, we reset v at the start of this function.
    ```
    bool has_unique_chars(const std::string &str, std::vector<bool> &v)
    ```
    
    Same idea as above, but std::bitset<256> will take up less space. This is the best version.
    ```
    bool has_unique_chars(const std::string &str, std::bitset<256> &bs)
    ```

--- src/01/01/main.cpp
+++ src/01/01/main.cpp
@@ -1,14 +1,25 @@
 /*!
     \file       main.cpp
-    \brief      Source file for application driver code
+    \brief      CTCI (6th Edition), question 1.1: Is Unique
+
+    \details
+    Implement an algorithm to determine if a string has all unique characters.
+    What if you cannot use additional data structures?
 
     \author     Gemuele Aludino
-    \date       15 Aug 2020
+    \date       21 Jan 2021
 */
 
 #include "header.hpp"
 
 #include <iostream>
+#include <vector>
+#include <bitset>
+
+bool has_unique_chars(const std::string &str, std::vector<bool> &v);
+bool has_unique_chars(const std::string &str, std::bitset<256> &bs);
+bool has_unique_chars(const std::string &str);
+
 /*!
     \brief      Program execution begins and ends here
 
@@ -18,6 +29,128 @@
     \return     0 on success, otherwise failure (see error code)
 */
 int main(int argc, const char *argv[]) {
-    std::cout << "Hello, world!" << std::endl;
+    auto strings = {
+        "pop",
+        "onetwothreefour",
+        "asdfjkl;",
+        "niNety",
+        "abBcX"
+    };
+
+    // Decimal ascii values range from [0, 128)
+    auto v = std::vector<bool>(128);
+    
+    std::cout << "Using std::vector<bool>(128): " << std::endl;
+    for (auto &s : strings) {
+        std::cout << s << " -> " << std::boolalpha << has_unique_chars(s, v) << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    // A char is an 1 byte (8-bit) integer 
+    auto bs = std::bitset<256>();
+
+    // Save space by using std::bitset<256>() as opposed to std::vector<bool>(128).
+    std::cout << "Using std::bitset<256>: " << std::endl;
+    for (auto &s : strings) {
+        std::cout << s << " -> " << std::boolalpha << has_unique_chars(s, bs) << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "No data structure: " << std::endl;
+    for (auto &s : strings) {
+        std::cout << s << " -> " << std::boolalpha << has_unique_chars(s) << std::endl;
+    }
+
     return EXIT_SUCCESS;
+}
+
+/*!
+    \brief      Determines if str is a string of all unique characters
+    \details    Uses std::vector<bool>(128)   
+    
+    \param[in]  str     The string to assess
+    \param[in]  v       Determines if a character has occurred more than once in str
+
+    \return             true, if str has all unique characters, false otherwise
+ */
+bool has_unique_chars(const std::string &str, std::vector<bool> &v) {
+    // We must reset the vector each time we run this function,
+    // since v will be reused. All elements assigned to be false, or 0.
+    v.assign(v.size(), false);
+
+    // Save the character count.
+    auto size = str.size();
+
+    for (auto i = 0; i < size; ++i) {
+        if (v[str[i]]) {
+            // if v at index str[i] == true
+            return false;
+        } else {
+            v[str[i]] = true;
+        }
+    }
+
+    return true;
+}
+
+/*!
+    \brief      Determines if str is a string of all unique characters
+    \details    Optimized method using std::bitset<256>          
+    
+    \param[in]  str     The string to assess
+    \param[in]  bs      Determines if a character has occurred more than once in str
+
+    \return             true, if str has all unique characters, false otherwise
+ */
+bool has_unique_chars(const std::string &str, std::bitset<256> &bs) {
+    // We must reset the bitset each time we run this function,
+    // since bs will be reused. All bits set to 0.
+    bs.reset();
+
+    // Save the character count.
+    auto size = str.size();
+
+    for (auto i = 0; i < size; ++i) {
+        if (bs[str[i]]) {
+            // if bs at index str[i] == true
+            return false;
+        } else {
+            // bs.set(str[i], true) also works here.
+            bs[str[i]] = true;
+        }
+    }
+    
+    return true;
+}
+
+/*!
+    \brief              Determines if str is a string of all unique characters
+    \details            Brute force method using nested loops.
+    \param[in]  str     The string to assess
+    \return             true, if str has all unique characters, false otherwise 
+ */
+bool has_unique_chars(const std::string &str) {
+    // Save the character count.
+    const auto size = str.size();
+
+    // The outer loop begins at i = 0;
+    // our current character is str[i].
+    for (auto i = 0; i < size - 1; ++i) {
+        // The inner loop begins at j = 1;
+        // our comparison character is str[j].
+        for (auto j = i + 1; j < size; ++j) {
+            // str[i] is compared with every other char after it.
+            if (str[i] == str[j]) {
+                // If the current character and comparison character
+                // are matched, we stop immediately and return false.
+                return false;
+            }
+        }
+    }
+
+    // Once we get this far, we know that str
+    // is composed of all unique chars.
+    return true;
 }
--- src/01/01/source.cpp
+++ src/01/01/source.cpp
@@ -1,11 +1,13 @@
 /*!
     \file       source.cpp
-    \brief      Source file
+    \brief      Source file for CTCI (6th Edition), question 1.1: Is Unique
+
+    \details
+    Implement an algorithm to determine if a string has all unique characters.
+    What if you cannot use additional data structures?
 
     \author     Gemuele Aludino
-    \date       15 Aug 2020
+    \date       21 Jan 2021
 */
 
 #include "header.hpp"
-
-
